00b8c31c235e4394f3b383f5a261dc49675bb7a6
Ankita Panda <ankitapanda08@gmail.com>
2020-08-09 14:15:52 +0530

    Maven project changes

---
+++ src/main/java/maven1_testing/App.java
@@ -0,0 +1,10 @@
+package maven1_testing;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+}
+}
---
+++ src/main/java/maven2_testing/App1.java
@@ -0,0 +1,10 @@
+package maven2_testing;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am a new user");
+	}
+
+}
